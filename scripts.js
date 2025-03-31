fae279ed9767f86616f5ce8eb086275613e95ffc
Marlonzito <marlonzitop@gmail.com>
2025-03-31 19:19:17 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".Starbucks")
+
+
+function TrocarCor(cor) {
+
+    circulo.style.background = cor
+
+}
+
+function trocarImagem(img) {
+    imagem.src = img
+}
